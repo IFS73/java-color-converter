b627990025a5965445255432eecf5c63295aa8ee
Moonpearl <romain.tello@gmail.com>
2021-07-31 17:25:52 +0200

    Add starter code

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,34 +3,30 @@ package com.example;
 import java.util.Arrays;
 
 /**
- * Hello world!
+ * Point d'entrée de l'application
  */
 public class App 
 {
+    /**
+     * Processus principal de l'applcation
+     * @param args
+     */
     public static void main( String[] args )
     {
+        // Transforme un code de couleur hexadécimal en rouge, vert, bleu, et affiche le résultat
         String hexString = "ffcc00";
-        int[] rgbArray = hexToRgb(hexString);
-        System.out.println( Arrays.toString(rgbArray) );
-
-        int red = 255;
-        int green = 204;
-        int blue = 0;
-        hexString = rgbToHex(red, green, blue);
-        System.out.println( hexString );
-    }
-
-    static int[] hexToRgb(String hexString)
-    {
-        return new int[] {
+        int[] rgbArray = new int[] {
             Integer.valueOf( hexString.substring( 0, 2 ), 16 ),
             Integer.valueOf( hexString.substring( 2, 4 ), 16 ),
             Integer.valueOf( hexString.substring( 4, 6 ), 16 ),
         };
-    }
+        System.out.println( Arrays.toString(rgbArray) );
 
-    static String rgbToHex(int red, int green, int blue)
-    {
-        return String.format("#%02X%02X%02X", red, green, blue);
+        // Transforme des valeurs de rouge, vert, bleu en code hexadécimal et affiche le résultat
+        int red = 255;
+        int green = 204;
+        int blue = 0;
+        hexString = String.format("#%02X%02X%02X", red, green, blue);
+        System.out.println( hexString );
     }
 }
